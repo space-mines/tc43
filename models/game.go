d2d708a028c566484b6768edaad126f8bfce28e5
Todd W Crone <twcrone@gmail.com>
2022-04-10 18:49:38 -0400

    Add a little more clarity in model

--- models/game.go
+++ models/game.go
@@ -1,24 +1,45 @@
 package models
 
-type asteroid struct {
-	Id        string `json:"id"`
-	Radiation int    `json:"radiation"`
-	Flagged   bool   `json:"flagged"`
+type mine struct {
+	x int
+	y int
+	z int
 }
 
-type game struct {
-	Id        string              `json:"id"`
-	Asteroids map[string]asteroid `json:"asteroids"`
-	mines     []string
+type Asteroid struct {
+	X         int  `json:"x"`
+	Y         int  `json:"y"`
+	Z         int  `json:"z"`
+	Radiation int  `json:"radiation"`
+	Flagged   bool `json:"flagged"`
 }
 
-var games = make(map[string]game)
+type Game struct {
+	Id        string     `json:"id"`
+	Asteroids []Asteroid `json:"asteroids"`
+	mines     []mine
+}
 
-func FindGameById(id string) game {
-	game := games[id]
-	if game.Asteroids == nil {
-		game.Asteroids = make(map[string]asteroid)
-		games[id] = game
+var games = make(map[string]Game)
+
+func FindGameById(id string) Game {
+	game, exists := games[id]
+	if !exists {
+		game = createNewGame(id, 3, 3)
 	}
+	return game
+}
+
+func createNewGame(id string, mineCount int, scale int) Game {
+	game := Game{Id: id, Asteroids: make([]Asteroid, scale*scale*scale)}
+	for x := 0; x < scale; x++ {
+		for y := 0; y < scale; y++ {
+			for z := 0; z < scale; z++ {
+				game.Asteroids = append(game.Asteroids, Asteroid{X: x, Y: y, Z: z, Radiation: -1, Flagged: false})
+			}
+		}
+	}
+	game.mines = []mine{{x: 1, y: 1, z: 1}}
+	games[id] = game
 	return game
 }
