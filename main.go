87fff300c938d8d881a8d02d19fcd9f226909760
Todd W Crone <twcrone@gmail.com>
2022-04-10 23:23:28 -0400

    Going back to what should work as it has worked before

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"log"
 	"os"
 
@@ -18,28 +19,12 @@ func main() {
 	}
 
 	router = gin.New()
+	router.Use(cors.Default())
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	initializeRoutes()
 
-	router.Use(cors())
 	router.Run(":" + port)
-}
-
-func cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
