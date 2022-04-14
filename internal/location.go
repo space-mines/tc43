0f1b828340925fef6f0dd6b98792f1e7e1b047df
Todd W Crone <twcrone@gmail.com>
2022-04-14 15:02:18 -0400

    Add couple more tests including location IS a mine

--- internal/location.go
+++ internal/location.go
@@ -16,11 +16,17 @@ func contains(locations []Location, x int, y int, z int) bool {
 }
 
 func (Location Location) CalculateRadiation(mines []Location) int {
+	if contains(mines, Location.X, Location.Y, Location.Z) {
+		return 43
+	}
 	radiation := 0
 	if contains(mines, Location.X-1, Location.Y-1, Location.Z-1) {
 		radiation++
 	}
 	if contains(mines, Location.X+1, Location.Y+1, Location.Z+1) {
+		radiation++
+	}
+	if contains(mines, Location.X+1, Location.Y+1, Location.Z) {
 		radiation++
 	}
 	return radiation
--- internal/location_test.go
+++ internal/location_test.go
@@ -10,6 +10,12 @@ import (
 var _ = Describe("Location", func() {
 	Describe("Checking radiation", func() {
 		location := internal.Location{X: 1, Y: 1, Z: 1}
+		Context("when location IS a mine", func() {
+			It("should be a 43", func() {
+				Expect(location.CalculateRadiation([]internal.Location{{X: 1, Y: 1, Z: 1}})).To(Equal(43))
+			})
+		})
+
 		Context("with no mines", func() {
 			It("should be a 0", func() {
 				Expect(location.CalculateRadiation([]internal.Location{})).To(Equal(0))
@@ -29,5 +35,13 @@ var _ = Describe("Location", func() {
 				Expect(location.CalculateRadiation(mines)).To(Equal(2))
 			})
 		})
+
+		Context("with three mines adjacent", func() {
+			It("should be a 3", func() {
+				mines := []internal.Location{{X: 0, Y: 0, Z: 0}, {X: 2, Y: 2, Z: 2}, {X: 1, Y: 1, Z: 2}}
+				Expect(location.CalculateRadiation(mines)).To(Equal(2))
+			})
+		})
+
 	})
 })
